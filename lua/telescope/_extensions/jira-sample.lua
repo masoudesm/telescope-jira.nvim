9294d4ef2682fe156e6605f6836e21d32d60345a
Masoud Esmaeilian <masoudesmaeilian@Masoud-Air.fritz.box>
2024-09-08 20:20:19 +0200

    Feat(): add stylua

--- lua/telescope/_extensions/jira-sample.lua
+++ lua/telescope/_extensions/jira-sample.lua
@@ -1,33 +1,33 @@
 local has_telescope, telescope = pcall(require, "telescope")
 if not has_telescope then
-	error("telescope-jira.nvim requires nvim-telescope/telescope.nvim")
+    error("telescope-jira.nvim requires nvim-telescope/telescope.nvim")
 end
 
 local has_plenary, plenary = pcall(require, "plenary")
 if not has_plenary then
-	error("telescope-jira.nvim requires plenary")
+    error("telescope-jira.nvim requires plenary")
 end
 
 local jira = require("jira-sample")
 
 local setup = function(config, _)
-	jira.telescope_config = config
+    jira.telescope_config = config
 
-	local level = "info"
-	if config.debug == true then
-		level = "debug"
-	end
+    local level = "info"
+    if config.debug == true then
+        level = "debug"
+    end
 
-	jira.log = plenary.log.new({
-		plugin = "Jira",
-		level = level,
-	})
+    jira.log = plenary.log.new({
+        plugin = "Jira",
+        level = level,
+    })
 end
 
 return telescope.register_extension({
-	setup = setup,
-	exports = {
-		jira_issue_picker = jira.jira_issue_picker,
-		jira_search_picker = jira.jira_search_picker,
-	},
+    setup = setup,
+    exports = {
+        jira_issue_picker = jira.jira_issue_picker,
+        jira_search_picker = jira.jira_search_picker,
+    },
 })
